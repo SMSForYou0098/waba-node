5551cdbfe0f3e1035001adff16f29e1712f453d3
smsforyou0098 <dev@smsforyou.biz>
2025-07-21 11:19:14 +0530

    error resolve

--- src/controllers/WebhookController.js
+++ src/controllers/WebhookController.js
@@ -1,10 +1,11 @@
-import { ErrorExtractor } from "../../utils/ErrorHandler"
+import { ErrorExtractor } from "../../utils/ErrorHandler.js";
 
-
-export const webHook = async(req,res)=>{
-    try {
-        console.log('req',req.body)
-    } catch (error) {
-        console.error('Error',ErrorExtractor(error));
-    }
+export const webHook = async (req, res) => {
+  try {
+    console.log('req', req.body);
+    res.status(200).send("Received"); // You should respond to avoid hanging connections
+  } catch (error) {
+    console.error('Error:', ErrorExtractor(error));
+    res.status(500).json({ error: ErrorExtractor(error) }); // Respond with error
+  }
 }
